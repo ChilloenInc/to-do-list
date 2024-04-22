cd6a291844a8cd87c0ea0d1f5947afda7eb6de34
yoon-jisung <hmsj2380@gmail.com>
2024-04-22 17:54:53 +0900

    add: alt 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,68 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import '../style/nav.css';
-import {images} from '../iconImage';
+import React, { useEffect, useState } from "react";
+import "../style/nav.css";
+import { images } from "../iconImage";
 
-const Navbar = ({ onSearchButtonClick, onSearch, isAdding, onAdd, onBack, onSave, isEditing, isSearching }) => {
-  
-  const [titleText, setTitleText] = useState('Todo');
+const Navbar = ({
+  onSearchButtonClick,
+  onSearch,
+  isAdding,
+  onAdd,
+  onBack,
+  onSave,
+  isEditing,
+  isSearching,
+}) => {
+  const [titleText, setTitleText] = useState("Todo");
 
   useEffect(() => {
     if (isAdding) {
-      setTitleText('Add');
-      if(isEditing) {
-        setTitleText('Edit');
+      setTitleText("Add");
+      if (isEditing) {
+        setTitleText("Edit");
       }
     } else {
-      setTitleText('Todo');
+      setTitleText("Todo");
     }
   }, [isAdding, isEditing]);
 
   return (
-    <nav className='nav'>
-
+    <nav className="nav">
       {isSearching ? (
-        <div className='search-wrap'>
-          <input 
-            type="text"
-            placeholder="검색"
-            onChange={onSearch}
-          />
+        <div className="search-wrap">
+          <input type="text" placeholder="검색" onChange={onSearch} />
         </div>
-      ): (
-        <span className='titleText'>{titleText}</span>
+      ) : (
+        <span className="titleText">{titleText}</span>
       )}
 
-    <div className='buttons'>
-      {!isSearching && !isAdding && (
+      <div className="buttons">
+        {!isSearching && !isAdding && (
           <button onClick={onSearchButtonClick}>
-            <img src={images.search}></img>
+            <img src={images.search} alt="icon"></img>
           </button>
-      )}
-      {
-        isSearching && (
+        )}
+        {isSearching && (
           <button onClick={onBack}>
-            <img src={images.back}></img>
+            <img src={images.back} alt="icon"></img>
           </button>
-        )
-      }
-      {!isSearching && !isAdding && (
+        )}
+        {!isSearching && !isAdding && (
           <button onClick={onAdd}>
-            <img src={images.add}></img>
+            <img src={images.add} alt="icon"></img>
           </button>
-      )}
-      {
-        isAdding && (
-        <>
-          <button onClick={onSave}>
-            <img src={images.submit}></img>
-          </button>
-          <button onClick={onBack}>
-            <img src={images.back}></img>
-          </button>
-        </>
-      )}
-    </div>
-  </nav>
+        )}
+        {isAdding && (
+          <>
+            <button onClick={onSave}>
+              <img src={images.submit} alt="icon"></img>
+            </button>
+            <button onClick={onBack}>
+              <img src={images.back} alt="icon"></img>
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
   );
 };
 

3857637f27bbeac5673861110364cc401a6f1c4a
yoon-jisung <hmsj2380@gmail.com>
2024-04-22 18:04:35 +0900

    bug: 리스트 선택 index로 된 부분 수정

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-import Navbar from './components/Nav';
-import Container from './components/Container';
-import '../src/style/form.css';
-import './style/index.css';
+import React, { useState } from "react";
+import Navbar from "./components/Nav";
+import Container from "./components/Container";
+import "../src/style/form.css";
+import "./style/index.css";
 
-const initTodo = [ 
+const initTodo = [
   {
-  id: 1,
-  title:'React',
-  body: 'React is',
+    id: 1,
+    title: "React",
+    body: "React is",
   },
   {
-  id: 2,
-  title: 'JS',
-  body: 'JS is',
+    id: 2,
+    title: "JS",
+    body: "JS is",
   },
   {
-  id: 3,
-  title:'Todo',
-  body: 'Todo something...',
+    id: 3,
+    title: "Todo",
+    body: "Todo something...",
   },
-]
+];
 // 추상황 -> 재사용성 up, 테스트하기 용이
-  // 리스트, 검색상태, 조회상태, 수정상태
-  // 리스트 = 할일을 보여주면 되고
-  // 검색상태 = 리스트에 필터건 할일 보여주면 되고
-  // 수정상태 = 선택한 할일 보여주면 되고
+// 리스트, 검색상태, 조회상태, 수정상태
+// 리스트 = 할일을 보여주면 되고
+// 검색상태 = 리스트에 필터건 할일 보여주면 되고
+// 수정상태 = 선택한 할일 보여주면 되고
 
-  // ! 이 방법이 베스트임. 함수 추상화 -> 함수형프로그래밍 -> 단점: 코드량 많아서
-  // ui - 비지니스 로직 분리를 하자.
-  //1. react-hooks(custom-hooks), 2.컴포넌트 단에서 나누는 방법
+// ! 이 방법이 베스트임. 함수 추상화 -> 함수형프로그래밍 -> 단점: 코드량 많아서
+// ui - 비지니스 로직 분리를 하자.
+//1. react-hooks(custom-hooks), 2.컴포넌트 단에서 나누는 방법
 
 const App = () => {
   const [todos, setTodos] = useState(initTodo);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const [editIndex, setEditIndex] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [editIndex, setEditTodoId] = useState(null);
   const [isAdding, setIsAdding] = useState(false);
   const [isSearching, setIsSearching] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
@@ -46,8 +46,8 @@ const App = () => {
   const handleSearchButtonClick = () => {
     setIsSearching(true);
     setIsAdding(false);
-  }
-  
+  };
+
   const handleSearchTerm = (e) => {
     setSearchTerm(e.target.value);
   };
@@ -58,13 +58,15 @@ const App = () => {
     setIsEditing(false);
   };
 
-  const handleTodoClick = (index) => {
+  const handleTodoClick = (id) => {
     setIsEditing(true);
     setIsAdding(true);
     setIsSearching(false);
-    setEditIndex(index);
-    setTitle(todos[index].title);
-    setBody(todos[index].body);
+    setEditTodoId(id);
+    const editTodo = todos.find((todo) => todo.id === id);
+    const { title, body } = editTodo;
+    setTitle(title);
+    setBody(body);
   };
 
   const handleBack = () => {
@@ -72,23 +74,25 @@ const App = () => {
       setIsSearching(false);
     } else {
       if (editIndex !== null) {
-        const confirmDelete = window.confirm('Are you sure you want to remove everything');
+        const confirmDelete = window.confirm(
+          "Are you sure you want to remove everything"
+        );
         if (confirmDelete) {
-          setEditIndex(null);
+          setEditTodoId(null);
           setIsAdding(false);
-          setTitle('');
-          setBody('');
+          setTitle("");
+          setBody("");
         }
       } else {
         setIsAdding(false);
-        setTitle('');
-        setBody('');
+        setTitle("");
+        setBody("");
       }
     }
   };
 
   const handleSave = () => {
-    if (title !== '' && body!== '') {
+    if (title !== "" && body !== "") {
       const updatedTodos = [...todos];
       if (editIndex !== null) {
         updatedTodos[editIndex] = { title, body };
@@ -98,9 +102,9 @@ const App = () => {
       }
       setTodos(updatedTodos);
       setIsAdding(false);
-      setEditIndex(null);
-      setTitle('');
-      setBody('');
+      setEditTodoId(null);
+      setTitle("");
+      setBody("");
     }
   };
 
@@ -109,7 +113,7 @@ const App = () => {
   );
 
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar
         onSearchButtonClick={handleSearchButtonClick}
         onSearch={handleSearchTerm}
@@ -121,27 +125,29 @@ const App = () => {
         onSave={handleSave}
       />
       {isAdding ? (
-        <div className='form'>
+        <div className="form">
           <input
-            className='form_title'
+            className="form_title"
             type="text"
             value={title}
             placeholder="Tittle"
             onChange={(e) => setTitle(e.target.value)}
           />
           <textarea
-            className='form_text'
+            className="form_text"
             value={body}
             placeholder="Description"
             onChange={(e) => setBody(e.target.value)}
           />
         </div>
       ) : (
-        <Container todos={filteredTodos} 
+        <Container
+          todos={filteredTodos}
           onTodoClick={handleTodoClick}
-          setEditIndex={setEditIndex} 
+          setEditIndex={setEditTodoId}
           onEdit={handleSave}
-          editIndex={editIndex}/>
+          editIndex={editIndex}
+        />
       )}
     </div>
   );
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,19 +1,17 @@
-import '../style/list.css'
+import "../style/list.css";
 
-const Container = ({ todos, onTodoClick}) => {
-
+const Container = ({ todos, onTodoClick }) => {
   return (
-    <div className='inner'>
-      <ul className='box_wrap'> 
-      {todos.map((todo, index) => (
-        <li 
-          key={index} onClick={() => onTodoClick(index)}>
-          <div className='items'>
-            <span className='list_title'>{todo.title}</span>
-            <p className='list_text'>{todo.body}</p>
-          </div>
-        </li>
-      ))}
+    <div className="inner">
+      <ul className="box_wrap">
+        {todos.map((todo, index) => (
+          <li key={`todos=${index}`} onClick={() => onTodoClick(todo.id)}>
+            <div className="items">
+              <span className="list_title">{todo.title}</span>
+              <p className="list_text">{todo.body}</p>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
